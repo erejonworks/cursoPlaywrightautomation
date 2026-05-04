2b9fb52bcbe0edf760ad9d435b4d3d62a4e3133d
edrejon89 <35882707+edrejon89@users.noreply.github.com>
2026-05-04 07:04:36 -0600

    Locators

--- playwright.config.js
+++ playwright.config.js
@@ -6,13 +6,14 @@ import { defineConfig, devices } from '@playwright/test';
  */
 const config = ({
   testDir: './tests',
-  timeout: 40*1000, 
+  timeout: 40*1000, // modificar timeouts generales
   expect:{
-    timeout:40*1000,
+    timeout:40*1000,// wait para los asserts.
   },
+  reporter: 'html',
   use: {
     browserName: 'chromium',
-    headless: true, // Propiedad para que siempre abra el navegador.
+    headless: false, // Propiedad para que siempre abra el navegador.
 
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,18 +1,24 @@
 const { test, expect } = require('@playwright/test'); // Se debe llamar esto para los assertions
 
-test.only('Browser Context Playwright test', async ({ browser }) => {// Se tiene que poner entre brackets los fixtures de Playwright para que operen bien. Sin ellos funciona como string.
+test.only('Browser Context Playwright test', async ({ browser }) => {// Se tiene que poner entre brackets los fixtures de Playwright para que operen bien. Sin ellos
+    //  funciona como string.
 
     // Context esta en la info de chrome plugins/ cookies
-    const context = await browser.newContext()//Crea una nueva instancia del navegador. Se pueden inyectar cookies.
-    const page = await context.newPage();
+    const context = await browser.newContext()//Crea una nueva instancia del navegador esta es limpia, como si fuera incognito. Se pueden inyectar cookies.
+    const page = await context.newPage(); // Crea la pagina
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+        // css, xpath 
+    await page.locator('#username').fill("rahulsheety");
+    await page.locator("[type='password']").fill("learning");
+    await page.locator('#signInBtn').click();
 });
 
 test('Page Playwright test', async ({ page }) => {// Se tiene que poner entre brackets los fixtures de Playwright para que operen bien. Sin ellos funciona como string.
     await page.goto("https://google.com");
     // get title - assertion
     console.log(await page.title());
-    await expect (page).toHaveTitle('Google');
+    await expect(page).toHaveTitle('Google');
 
 
 });
