3683f31fe550c6b702ca645911301ba7f801c63a
edrejon89 <35882707+edrejon89@users.noreply.github.com>
2026-05-14 07:00:56 -0600

    Update RegisterTest.spec.js

--- tests/RegisterTest.spec.js
+++ tests/RegisterTest.spec.js
@@ -1,6 +1,33 @@
 const {test, expect} = require('@playwright/test');
 
 test('Registration on the site', async({page}) => {
+
+     // Locators
+    // Login Form
+    const signUpLink = page.locator("a[class='text-reset']");
+    const inputEmail = page.locator("#userEmail");
+    const inputPwd = page.locator("[type='password']");
+    const btnSignIn = page.locator("input[name='login']");
+    // Sign up form
+    const fName = page.locator("#firstName");
+    const lName = page.locator("#lastName");
+    const signUpEmail = page.locator("[type='email']");
+    const inputUserMobile = page.locator("#userMobile");
+    const inputGender = page.locator('input[value="Male"]');
+    const inputpwd = page.locator("#userPassword");
+    const inputpwdconfirm = page.locator("#confirmPassword");
+    const = page.locator("");
+    const = page.locator("");
+
+    //Actions
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-    
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Let's Shop");
+
+    // Registration
+    await signUpLink.click();
+    await
+
+    //login
+    await inputEmail.fill("")
 });
